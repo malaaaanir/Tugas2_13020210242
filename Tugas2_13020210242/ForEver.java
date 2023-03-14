67d5c9cee574c0c0ea32cc1a2d5818ef132f34d0
malaaaanir <107983883+malaaaanir@users.noreply.github.com>
2023-03-14 18:37:07 -0300

    Add files via upload

---
+++ Tugas2_13020210242/ForEver.java
@@ -0,0 +1,14 @@
+// 13020210242 Nirmala
+// Rabu, 15 Maret 2023
+public class ForEver {
+    public static void main(String args[]) {
+        // Program
+        System.out.println("Program Akan Looping, Akhiri Dengan ^c");
+        // Perintah output teks
+        while(true){
+        // Kondisi jika bernilai true
+            System.out.print("Print Satu Baris ...\n");
+            // Perintah output teks
+        }
+    }
+}
